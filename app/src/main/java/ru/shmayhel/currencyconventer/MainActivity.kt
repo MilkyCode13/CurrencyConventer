8f380be1d5bad8f950f8b6b59c67878d415ec326
MilkyCode13 <shmayhel.andrey@gmail.com>
2020-06-04 13:42:46 +0300

    First stage completed (RUB to USD)

--- app/src/main/java/ru/shmayhel/currencyconventer/MainActivity.kt
+++ app/src/main/java/ru/shmayhel/currencyconventer/MainActivity.kt
@@ -2,10 +2,30 @@ package ru.shmayhel.currencyconventer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firstCurrencyNumberInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                try {
+                    secondCurrencyNumberView.text =
+                        String.format("%.2f", convert(firstCurrencyNumberInput.text.toString().toDouble()))
+                }
+                catch (nfe: NumberFormatException) {}
+            }
+        })
+    }
+
+    private fun convert(firstCurrencyNumber: Double): Double {
+        return firstCurrencyNumber / 74
     }
 }
