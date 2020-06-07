59a9b1a5adb394e4b7c00588f434d03bdbf64197
MilkyCode13 <shmayhel.andrey@gmail.com>
2020-06-07 13:14:32 +0300

    Third stage completed (Between multiple currencies)

--- app/src/main/java/ru/shmayhel/currencyconventer/MainActivity.kt
+++ app/src/main/java/ru/shmayhel/currencyconventer/MainActivity.kt
@@ -16,18 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val spinnerAdapter = ArrayAdapter(this, R.layout.spinner_custom, resources.getStringArray(R.array.currency_list))
-        currencySpinner.adapter = spinnerAdapter
-        currencySpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                if(position == 0)
-                    secondCurrencyTextView.text = resources.getStringArray(R.array.currency_list)[1]
-                else
-                    secondCurrencyTextView.text = resources.getStringArray(R.array.currency_list)[0]
+        firstCurrencySpinner.adapter = spinnerAdapter
+        secondCurrencySpinner.adapter = spinnerAdapter
+        firstCurrencySpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                updateNumber()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+        secondCurrencySpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 updateNumber()
             }
 
@@ -46,16 +45,23 @@ class MainActivity : AppCompatActivity() {
     private fun updateNumber() {
         try {
             val firstCurrencyNumber = firstCurrencyNumberInput.text.toString().toDouble()
-            secondCurrencyNumberView.text =
-                String.format("%.2f", convert(firstCurrencyNumber, currencySpinner.selectedItemPosition))
+            val secondCurrencyNumber = convert(firstCurrencyNumber, firstCurrencySpinner.selectedItemPosition, secondCurrencySpinner.selectedItemPosition)
+            if(secondCurrencyNumber < 0.1)
+                secondCurrencyNumberView.text = String.format("%.4f", secondCurrencyNumber)
+            else
+                secondCurrencyNumberView.text = String.format("%.2f", secondCurrencyNumber)
         }
         catch (nfe: NumberFormatException) {}
     }
 
-    private fun convert(firstCurrencyNumber: Double, firstCurrencyID: Int): Double {
-        if(firstCurrencyID == 0)
-            return firstCurrencyNumber / 74
-        else
-            return firstCurrencyNumber * 74
+    private fun convert(firstCurrencyNumber: Double, firstCurrencyID: Int, secondCurrencyID: Int): Double {
+        val exchangeRates: Array<Double> = arrayOf(69.0, 1.0, 0.89, 0.79, 0.96, 27.0, 24000.0)
+        return firstCurrencyNumber * exchangeRates[secondCurrencyID] / exchangeRates[firstCurrencyID]
+    }
+
+    fun swapCurrencies(view: View) {
+        var swapPos = secondCurrencySpinner.selectedItemPosition
+        secondCurrencySpinner.setSelection(firstCurrencySpinner.selectedItemPosition)
+        firstCurrencySpinner.setSelection(swapPos)
     }
 }
